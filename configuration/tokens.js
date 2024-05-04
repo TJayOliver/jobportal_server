138eca06451de6df158f2f651068e938ed0d1e15
T-Jay Oliver <126061191+TJayOliver@users.noreply.github.com>
2024-05-04 04:26:38 -0800

    Update tokens.js

--- configuration/tokens.js
+++ configuration/tokens.js
@@ -21,7 +21,7 @@ export const retrieveToken = async (req, res, next) => {
 
 export const verifyToken = async (cookie) => {
   try {
-    const decoded = jwt.decode(cookie, process.env._c);
+    const decoded = jwt.decode(cookie, process.env.SK_KEY2);
     if (decoded) {
       const country = decoded.country;
       return { country: country };
