0b52ca12a920c5b81d790d49e5c1b56699074f76
T-Jay Oliver <126061191+TJayOliver@users.noreply.github.com>
2024-05-04 06:33:45 -0800

    Update mongodb.config.js

--- configuration/mongodb.config.js
+++ configuration/mongodb.config.js
@@ -3,21 +3,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const connectMongoDB = async () => {
-  if (process.env.NODE_ENV === "development") {
-    const url = process.env.MONGODB_URL;
-    try {
-      await mongoose.connect(url);
-      console.log(`connected to database:${url}`);
-    } catch (error) {
-      console.error(error.message);
-    }
-  } else {
     const url = process.env.MONGO_ATLAS;
     try {
       await mongoose.connect(url);
-      console.log(`connected to mongodb `);
+      console.log(`connected to mongodb`);
     } catch (error) {
       console.error(error.message);
     }
-  }
 };
